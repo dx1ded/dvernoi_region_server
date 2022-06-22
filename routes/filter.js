fba7a5d9c047fb062e669939d4640a49e0c5ffa3
Volodymyr Doskochinskiy <doskovol1@gmail.com>
2022-06-22 18:40:03 -0300

    123

--- routes/filter.js
+++ routes/filter.js
@@ -8,14 +8,13 @@ export const filter = Router()
 
 filter.get("/getBrands", async (req, res) => {
   try {
-    const type = req._parsedUrl.pathname.substring(1)
     const queryParams = url.parse(req.url, true).query
 
-    if (!type || !queryParams.key || !queryParams.value) {
+    if (!queryParams.type || !queryParams.key || !queryParams.value) {
       return res.status(400).send("Неправильные данные")
     }
 
-    const { key, value } = queryParams
+    const { type, key, value } = queryParams
 
     const types = {
       "doors": Doors,
